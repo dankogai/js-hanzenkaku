bb195eeb40bcc64d7b684927835196300c965d0d
Dan Kogai <dankogai+github@gmail.com>
2012-08-13 18:02:34 +0900

    tests now work w/ browsers

--- test/dankogai.js
+++ test/dankogai.js
@@ -1,27 +1,30 @@
+
 /*
  * $Id: dankogai.js,v 0.1 2012/08/13 05:30:12 dankogai Exp dankogai $
  *
  * use mocha to test me
  * http://visionmedia.github.com/mocha/
  */
-
-var assert = require("assert"),
-    is = function (a, e, m) {
-        return function () {
-            assert.equal(a, e, m)
-        }
-    },
-    hzk = require('../hanzenkaku.js').HanZenKaku;
+var assert, HanZenKaku;
+if (this['window'] !== this) {
+    assert = require("assert");
+    HanZenKaku = require('../hanzenkaku.js').HanZenKaku;
+}
+var is = function (a, e, m) {
+    return function () {
+        assert.equal(a, e, m)
+    }
+};
 
 describe('HanZenKaku', function () {
-    it('.z2h', is(hzk.z2h('コガイダン'), 'ｺｶﾞｲﾀﾞﾝ'));
-    it('.h2z', is(hzk.h2z('ｺｶﾞｲﾀﾞﾝ'), 'コガイダン'));
-    it('.fw2hw', is(hzk.fw2hw('ｄａｎｋｏｇａｉ'), 'dankogai'));
-    it('.hw2fw', is(hzk.hw2fw('dankogai'), 'ｄａｎｋｏｇａｉ'));
-    it('.fs2hs', is(hzk.fs2hs('dan　kogai'), 'dan kogai'));
-    it('.hs2fs', is(hzk.hs2fs('dan kogai'), 'dan　kogai'));
-    it('.h2k', is(hzk.h2k('こがいだん'), 'コガイダン'));
-    it('.k2h', is(hzk.k2h('コガイダン'), 'こがいだん'));
+    it('.z2h', is(HanZenKaku.z2h('コガイダン'), 'ｺｶﾞｲﾀﾞﾝ'));
+    it('.h2z', is(HanZenKaku.h2z('ｺｶﾞｲﾀﾞﾝ'), 'コガイダン'));
+    it('.fw2hw', is(HanZenKaku.fw2hw('ｄａｎｋｏｇａｉ'), 'dankogai'));
+    it('.hw2fw', is(HanZenKaku.hw2fw('dankogai'), 'ｄａｎｋｏｇａｉ'));
+    it('.fs2hs', is(HanZenKaku.fs2hs('dan　kogai'), 'dan kogai'));
+    it('.hs2fs', is(HanZenKaku.hs2fs('dan kogai'), 'dan　kogai'));
+    it('.h2k', is(HanZenKaku.h2k('こがいだん'), 'コガイダン'));
+    it('.k2h', is(HanZenKaku.k2h('コガイダン'), 'こがいだん'));
 });
 
 describe('String.prototype', function () {
